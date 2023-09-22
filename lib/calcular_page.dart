fcea586fae2a17a8c018dfb1038b4ff67980e608
kathyLopezSMOKE <Katherine Lopez>
2023-09-22 07:59:24 -0500

    slider

--- lib/calcular_page.dart
+++ lib/calcular_page.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class calcular extends StatelessWidget {
+class calcular extends StatefulWidget {
   const calcular({super.key});
+
+  @override
+  State<calcular> createState() => _calcularState();
+}
+
+class _calcularState extends State<calcular> {
+double valuePeso=0;
+double valueAltura=0;
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +20,41 @@ class calcular extends StatelessWidget {
         centerTitle: false,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(10.0),
         child: Column(
           children: [
-            Text('Bienvenido, seleccione su peso y altura'),
+            Container(
+              alignment: Alignment.bottomLeft,
+              child: Text('Bienvenido, seleccione su peso y altura'),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text("111 kg"),
+            Slider(
+              min: 0,
+              max: 250,
+              value: valuePeso,
+              onChanged: (value){
+                print(value);
+                valuePeso=value;
+                setState(() {});
+              },
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text("180 cm"),
+            Slider(
+              min: 0,
+              max: 250,
+              value: valueAltura,
+              onChanged: (value){
+                print(value);
+                valueAltura=value;
+                setState(() {});
+              },
+            ),
           ],
         ),
       ),
