95f5b29a84bf75bdadf0403d81875d2191654d8b
kathyLopezSMOKE <Katherine Lopez>
2023-09-22 19:31:36 -0500

    boton_estilo

--- lib/calcular_page.dart
+++ lib/calcular_page.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class calcular extends StatefulWidget {
@@ -11,6 +13,13 @@ class _calcularState extends State<calcular> {
 double valuePeso=0;
 double valueAltura=0;
 
+void calcularIMC(){
+  setState(() {
+    double altura=valueAltura/100;
+    double peso= valuePeso;
+    double imc=peso/(altura*altura);
+  });
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +34,7 @@ double valueAltura=0;
           children: [
             Container(
               alignment: Alignment.bottomLeft,
-              child: Text('Bienvenido, seleccione su peso y altura'),
+              child: Text('Bienvenido, seleccione su peso y altura',style: TextStyle(fontSize: 17),),
             ),
             SizedBox(
               height: 40,
@@ -103,10 +112,23 @@ double valueAltura=0;
                 setState(() {valueAltura=value;});
               },
             ),
-            ElevatedButton(
-              onPressed: (){}, 
+            SizedBox(
+              height: 30,
+            ),            
+               Container(
+                width: double.infinity,
+                height: 50,
+                color: Color.fromARGB(255, 60, 60, 60),
+                child: ElevatedButton(
+              onPressed: (){
+                calcularIMC();
+              }, 
               child: const Text("Calcular"),
-            ),
+              style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 155, 35, 147),
+              ),
+            ) ,
+              ),
           ],
         ),
       ),
